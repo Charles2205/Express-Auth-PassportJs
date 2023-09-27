e3a3b2d4ff1abd6fa9c1e2154190b2bd6db391e7
Charles2205 <cewudzie0@gmail.com>
2023-09-27 12:07:56 +0000

    Config DB

--- dbConnect.js
+++ dbConnect.js
@@ -1,1 +1,14 @@
 const {Sequelize} = require('sequelize')
+require('dotenv').config()
+const DB_NAME = process.env.DB_NAME
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+
+const sequel = new Sequelize(DB_NAME,DB_USER,DB_PASSWORD,{
+    host:DB_HOST
+,    dialect:'mssql'
+})
+
+module.exports = sequel
