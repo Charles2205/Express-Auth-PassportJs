ffdfe0f4e5acb96882e2bb187343a3278b85e388
Charles2205 <cewudzie0@gmail.com>
2023-09-27 12:34:52 +0000

    Update: Created tables

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT
 const dbConnect = require('./dbConnect')
+const dbModel = require('./user')
 const app = express()
 
 
